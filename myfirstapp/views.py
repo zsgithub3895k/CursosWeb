2bf20b706e281ee76af769874646951fe505d109
zsgithub3895k <wuwangrh144309@yeah.net>
2021-06-02 00:14:50 +0800

    Pasando PEP8

--- myfirstapp/views.py
+++ myfirstapp/views.py
@@ -1,11 +1,18 @@
 # Create your views here.
-
 from django.http import HttpResponse
+
+
 def say_main(request):
-     return HttpResponse('<h1>My First Application</h1>')
+    return HttpResponse('<h1>My First Application</h1>')
+
+
 def say_hello(request):
-     return HttpResponse('Hello!')
+    return HttpResponse('Hello!')
+
+
 def say_bye_to(request, name):
-     return HttpResponse('Bye %s'%name)
+    return HttpResponse('Bye %s' %name)
+
+
 def say_number(request, number=0):
-     return HttpResponse('Number: %s'%number)
+    return HttpResponse('Number: %s' %number)
